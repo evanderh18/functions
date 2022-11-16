fb4955eb7fdc0c6e2e21eee502b5975818d300e1
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2022-11-15 19:12:28 -0700

    cond statement for unit

--- functions.py
+++ functions.py
@@ -13,7 +13,13 @@ user_unit = input("What unit is your number?" )
 # to convert mm to in --> mm / 25.4
 
 # user gives in unit
-converted_number = user_number * 25.4
-
+#converted_number = user_number * 25.4
+converted_number = user_number / 25.4
+if(user_unit == 'in'):
+    #perform in to mm
+    converted_number = user_number * 25.4
+if(user_unit == 'mm'):
+    #performmm to in
+    converted_number = user_number / 25.4
 print(converted_number)
 print(user_unit)
