a917ef13f1918f46424ca692e4d4d006408722b0
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2022-11-17 18:13:46 -0700

    addedloop

--- functions.py
+++ functions.py
@@ -5,18 +5,21 @@
 # get the unit from the user
 # convert the length to the correct unit
 # output the answer to the user
+while True:
+    user_number = float(input("What number to convert? "))
+    user_unit = input("What unit is your number?" )
 
-user_number = float(input("What number to convert? "))
-user_unit = input("What unit is your number?" )
+    if(user_unit == 'in'):
+        #perform in to mm
+        converted_number = user_number * 25.4
+        conv_unit = 'mm'
+        break
+    if(user_unit == 'mm'):
+        #performmm to in
+        converted_number = user_number / 25.4
+        conv_unit = 'in'
+        break
+    else:
+        print('That is not a valid unit')
 
-if(user_unit == 'in'):
-    #perform in to mm
-    converted_number = user_number * 25.4
-    conv_unit = 'mm'
-if(user_unit == 'mm'):
-    #performmm to in
-    converted_number = user_number / 25.4
-    conv_unit = 'in'
-else:
-    print('That is not a valid unit')
 print(converted_number, conv_unit)
